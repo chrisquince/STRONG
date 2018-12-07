cfd9012260c58947585eb193eab70fb1db3fed04
Sebastien-Raguideau <idaho90@live.fr>
2018-12-07 22:21:57 +0000

    fixes faa to fna

---
+++ COG_pipe/scripts/get_num_bin_ini.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.5
+import argparse 
+import numpy as np
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+from collections import Counter
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("SCG_annotation",help="")
+  args = parser.parse_args()
+  # we take a look at all the SCG, take the median over the 36 of them, and multiply that by 10
+  print(str(int(10*np.median(list(Counter([header.split(" ")[1] for header,seq in SimpleFastaParser(open(args.SCG_annotation))]).values())))))
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+

dfdfe4d4a234afee828e1dac32af63ab6e991c77
Sebastien-Raguideau <idaho90@live.fr>
2019-01-11 18:29:24 +0000

    added cog database in config

--- COG_pipe/scripts/Rpsblast_loop.sh
+++ COG_pipe/scripts/Rpsblast_loop.sh
@@ -3,12 +3,14 @@
 
 TEMP_FOLDER=$1
 THREAD_CNT=$2
+DB=$3
 
 function launch_rpsblast {
     file=$1
     base=${file%.faa}
+    DB=$2
     Output=$base"_Rpsblast.tsv"
-    rpsblast+ -outfmt '6 qseqid sseqid evalue pident length slen qlen' -evalue 0.00001 -query $file -db /home/sebr/seb/Database/rpsblast_cog_db/Cog -out $Output
+    rpsblast+ -outfmt '6 qseqid sseqid evalue pident length slen qlen' -evalue 0.00001 -query $file -db $DB -out $Output
     return 0
 }
 
@@ -16,7 +18,7 @@ function launch_rpsblast {
 List_files=$( ls $TEMP_FOLDER/*.faa )
 
 export -f launch_rpsblast
-echo $List_files | tr -s " " "\n" | xargs -n 1 -P $THREAD_CNT -I {} bash -c 'launch_rpsblast "$@"' _ {}
+echo $List_files | tr -s " " "\n" | xargs -n 1 -P $THREAD_CNT -I {} bash -c 'launch_rpsblast "$@"' _ {} $DB
 
 
 cat $TEMP_FOLDER/*_Rpsblast.tsv > $TEMP_FOLDER/Complete_Rpsblast_cogs.tsv

79d220f6b21fef5bb2e7b505d8f3272c75e34a1f
Seb <idaho90@live.fr>
2019-01-25 16:02:24 +0000

    add scg_data folder

--- COG_pipe/scripts/common.py
+++ COG_pipe/scripts/common.py
@@ -30,6 +30,7 @@ def fill_default_values(config):
     if local_dir:
         default_values["bin"] = os.path.join(local_dir, "build/bin")
         default_values["scripts"] = os.path.join(local_dir, "scripts")
+        default_values["scg_data"]= os.path.join(local_dir, "scg_data")
     setdefault_recursively(config)
 
 def sample_name(fullname):

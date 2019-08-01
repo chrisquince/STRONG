dc521c5a23ba5aa49c72920af8fd238f6666b050
Sebastien-Raguideau <idaho90@live.fr>
2019-08-01 19:18:37 +0100

    issue with upper limit of files open

--- COG_pipe/scripts/Split_fasta_by_bin.py
+++ COG_pipe/scripts/Split_fasta_by_bin.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
+from __future__ import division
 import argparse,os
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from collections import defaultdict
+import resource
 
 def  main(fasta_file,bin_composition,set_bins,output):
-	if set_bins :
-		Dico_contigs_bin={line.rstrip().split(",")[0]:line.rstrip().split(",")[1] for line in open(bin_composition) if line.rstrip().split(",")[1] in set_bins}
-	else :
-		Dico_contigs_bin={line.rstrip().split(",")[0]:line.rstrip().split(",")[1] for line in open(bin_composition)}
-	Dico_bin_Handle={}
-	for bins in set(Dico_contigs_bin.values()) :
-		Dico_bin_Handle[bins]=open(output+"/Bin_"+bins+"."+fasta_file.split(".")[-1],"w")
-	for contig_id,seq in SimpleFastaParser(open(fasta_file)) :
-		contig_id2=contig_id.split()[0]
-		if contig_id2 in Dico_contigs_bin :
-			Dico_bin_Handle[Dico_contigs_bin[contig_id2]].write(">"+contig_id+"\n"+seq+"\n")
-	for handle in Dico_bin_Handle.values() :
-		handle.close()
+    Dico_contigs_bin={line.rstrip().split(",")[0]:line.rstrip().split(",")[1] for line in open(bin_composition) if line.rstrip().split(",")[1] in set_bins}
+    Dico_bin_Handle={}
+    for bins in set(Dico_contigs_bin.values()) :
+        Dico_bin_Handle[bins]=open(output+"/Bin_"+bins+"."+fasta_file.split(".")[-1],"w")
+    for contig_id,seq in SimpleFastaParser(open(fasta_file)) :
+        contig_id2=contig_id.split()[0]
+        if contig_id2 in Dico_contigs_bin :
+            Dico_bin_Handle[Dico_contigs_bin[contig_id2]].write(">"+contig_id+"\n"+seq+"\n")
+    for handle in Dico_bin_Handle.values() :
+        handle.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,13 +28,17 @@ if __name__ == "__main__":
     fasta_file=args.fasta_file
     bin_composition=args.bin_composition
     path_output=args.path_output
-    set_bins=set([])
-    if args.l :
-        set_bins=set(args.l)
-    main(fasta_file,bin_composition,set_bins,path_output)
-
-
-
-
-
-
+    #---------- Batch strategy ----------------
+    # there is a hard limit of number of handles which can be opened
+    max_nb_handles=resource.getrlimit(resource.RLIMIT_NOFILE)[0]
+    max_nb_handles=max_nb_handles-100
+    List_all_bins=list({line.rstrip().split(",")[1] for line in open(bin_composition)})
+    Nb_bins=len(List_all_bins)
+    nb_batch=Nb_bins//max_nb_handles+1
+    List_Batchs=[List_all_bins[batch*max_nb_handles:min((batch+1)*max_nb_handles,Nb_bins)] for batch in range(nb_batch)]
+    for Batch in List_Batchs :
+        if args.l :
+            set_bins=set(Batch)&set(args.l)
+        else :
+            set_bins=set(Batch)
+        main(fasta_file,bin_composition,set_bins,path_output)

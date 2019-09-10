083aa11e67a5c81dd3519fab4ef8c7ea471df97e
Sebastien-Raguideau <idaho90@live.fr>
2019-09-10 16:48:50 +0100

    all the change, at once

--- COG_pipe/scripts/evaluation/AddStrainsCov.py
+++ COG_pipe/scripts/evaluation/AddStrainsCov.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+from __future__ import print_function
 import sys
+import re
 import argparse
 import numpy as np
 from collections import defaultdict
 
-#0  663 1219076_0   0.212580070954  0.000583505311272
+# 0  663 1219076_0   0.212580070954  0.000583505311272
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -19,18 +23,17 @@ def main(argv):
 #    import ipdb; ipdb.set_trace()
 
     species_strains = defaultdict(set)
-    strain_covs = defaultdict(lambda: np.zeros(96)) 
+    strain_covs = defaultdict(lambda: np.zeros(96))
 
     with open(args.cov_file) as sin:
-    
-        for line in sin:
-            
-            line = line.rstrip()
-            
-            (sample, species, strain, cov, frac) = line.split('\t')
-       
 
-            strain_covs[strain][int(sample)] = float(cov) 
+        for line in sin:
+
+            line = line.rstrip()
+
+            (sample, species, strain, cov, frac) = line.split('\t')
+
+            strain_covs[strain][int(sample)] = float(cov)
             species_strains[species].add(strain)
 
     bFirst = True
@@ -53,7 +56,9 @@ def main(argv):
                     total_covs.append(np.sum(strain_covs[strain][0:32]))
                 nameString = ",".join(strains)
                 covString = ",".join([str(x) for x in total_covs])
-                print(cluster + "," + species + "," + prob + "," + str(nStrains) + "," + nameString + "," + covString) 
+                print(re.sub("^D","Bin_",cluster) + "," + species + "," + prob + "," +
+                      str(nStrains) + "," + nameString + "," + covString)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- COG_pipe/scripts/evaluation/MapCounts.py
+++ COG_pipe/scripts/evaluation/MapCounts.py
@@ -1,51 +1,43 @@
-import sys, getopt
-import os
-import pandas as p
-import numpy as np
-import scipy.stats as ss
-import scipy as sp
-import scipy.misc as spm
-import math
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
 import argparse
-import cPickle
-import logging
-import json
 import glob
 import re
-import subprocess
-import gzip
-import atexit
 import operator
 
-from pprint import pprint
 from Bio import SeqIO
-from random import shuffle
 from collections import defaultdict
 from collections import Counter
-from numpy.random import RandomState
+
 
 def main(argv):
 
     parser = argparse.ArgumentParser()
-    
-    parser = argparse.ArgumentParser(description='Simulate a metagenomic data set')
-    
-    parser.add_argument("genomes_dir", help="directory with genomes")
+
+    parser = argparse.ArgumentParser(
+        description='Simulate a metagenomic data set')
+
+    parser.add_argument("genomes_dir", help="directory with genomes. Warning : all file in this folder should be genomes fasta files")
 
     parser.add_argument("select_file", help="mapping strains to species")
 
     parser.add_argument("count_file", help="count file")
 
+    parser.add_argument("output_strain", help="output path for strain file")
+
+    parser.add_argument("output_species", help="output path for species file")
+
     parser.add_argument("--amb", help="use ambiguous reads",
-                    action="store_true")
+                        action="store_true")
 
     args = parser.parse_args()
 
 #    import ipdb; ipdb.set_trace()
 
     mapStrain = {}
-    
-    for fasta_file in glob.glob(args.genomes_dir + "/*seq.tmp"):
+
+    for fasta_file in glob.glob(args.genomes_dir + "/*"):
         seq_records = list(SeqIO.parse(fasta_file, "fasta"))
 
         m = re.search(r"(\d+_\d+)seq.tmp", fasta_file)
@@ -54,32 +46,30 @@ def main(argv):
 
         for record in seq_records:
             mapStrain[record.id] = strain_id
-    
-        print "Read fasta ids for:" + str(strain_id)
 
+        # print("Read fasta ids for:" + str(strain_id))
 
     mapSpecies = {}
     for line in open(args.select_file):
         line = line.rstrip()
-            
-        (species, strain, nSeqs, dir) = line.split("\t") 
+
+        (species, strain, nSeqs, dir) = line.split("\t")
         mapSpecies[strain] = species
-    
-    countContigStrainAmb  = defaultdict(Counter)
-    countContigStrainUnamb  = defaultdict(Counter)
-        
+
+    countContigStrainAmb = defaultdict(Counter)
+    countContigStrainUnamb = defaultdict(Counter)
 
     countContigSpeciesAmb = defaultdict(Counter)
     countContigSpeciesUnamb = defaultdict(Counter)
 
-    strain_file = open("Strain.csv", "w")
-    species_file = open("Species.csv", "w")
+    strain_file = open(args.output_strain, "w")
+    species_file = open(args.output_species, "w")
 
     header = True
     mapIdx = {}
     for line in open(args.count_file):
         line = line.rstrip()
-    
+
         if header:
             names = line.split("\t")
             header = False
@@ -87,23 +77,24 @@ def main(argv):
             names.pop(0)
             for name in names:
                 m = re.search(r"(.*)_read_count_(.*)", name)
-                
+
                 amb = False
                 if m.group(1) == 'amb':
                     amb = True
-                mapIdx[idx] = (amb,m.group(2)) 
-                idx = idx + 1 
+                mapIdx[idx] = (amb, m.group(2))
+                idx = idx + 1
         else:
             counts = line.split("\t")
             contig = counts.pop(0)
             idx = 0
             total = 0
+            counts=map(int,counts)
             
             countSpecies = Counter()
             countStrain = Counter()
             for count in counts:
-                (amb,seqId) = mapIdx[idx]
-                strainId  = mapStrain[seqId]
+                (amb, seqId) = mapIdx[idx]
+                strainId = mapStrain[seqId]
                 speciesId = mapSpecies[strainId]
 
                 if count > 0:
@@ -116,54 +107,61 @@ def main(argv):
                         countSpecies[speciesId] += int(count)
 
                     if amb:
-                        countContigStrainAmb[contig][strainId]  += int(count)
+                        countContigStrainAmb[contig][strainId] += int(count)
                         countContigSpeciesAmb[contig][speciesId] += int(count)
                         if args.amb:
                             total += int(count)
                     else:
                         total += int(count)
-                        countContigStrainUnamb[contig][strainId]  += int(count)
-                        countContigSpeciesUnamb[contig][speciesId] += int(count)         
-                
+                        countContigStrainUnamb[contig][strainId] += int(count)
+                        countContigSpeciesUnamb[contig][speciesId] += int(
+                            count)
+
                 idx = idx + 1
-            
-            sorted_strains = sorted(countStrain.items(), key=operator.itemgetter(1),reverse=True)
-            sorted_species = sorted(countSpecies.items(), key=operator.itemgetter(1),reverse=True)            
-            
+
+            sorted_strains = sorted(
+                countStrain.items(), key=operator.itemgetter(1), reverse=True)
+            sorted_species = sorted(countSpecies.items(
+            ), key=operator.itemgetter(1), reverse=True)
+
             assignedStrains = []
             assignedString = ""
             if total > 0:
                 for strain_tuple in sorted_strains:
                     (strain, count) = strain_tuple
-        
+
                     if count > 0:
                         frac = float(count)/float(total)
-                        assignedStrains.append((strain,count,frac))
-                        assignedString += "," + ','.join(map(str, [strain,count,frac])) 
+                        assignedStrains.append((strain, count, frac))
+                        assignedString += "," + \
+                            ','.join(map(str, [strain, count, frac]))
             nAssigned = len(assignedStrains)
-            
-            strain_file.write(contig + "," + str(total) + "," + str(nAssigned))            
+
+            strain_file.write(contig + "," + str(total) + "," + str(nAssigned))
             strain_file.write(assignedString)
             strain_file.write("\n")
 
             assignedSpecies = []
-            assignedString  = ""
+            assignedString = ""
             if total > 0:
                 for species_tuple in sorted_species:
                     (species, count) = species_tuple
 
                     if count > 0:
                         frac = float(count)/float(total)
-                        assignedSpecies.append((species,count,frac))
-                        assignedString += "," + ','.join(map(str, [species,count,frac]))
+                        assignedSpecies.append((species, count, frac))
+                        assignedString += "," + \
+                            ','.join(map(str, [species, count, frac]))
             nAssigned = len(assignedSpecies)
 
-            species_file.write(contig + "," + str(total) + "," + str(nAssigned))
+            species_file.write(contig + "," + str(total) +
+                               "," + str(nAssigned))
             species_file.write(assignedString)
             species_file.write("\n")
 
     strain_file.close()
     species_file.close()
-    
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])

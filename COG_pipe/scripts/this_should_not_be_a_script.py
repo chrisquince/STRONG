17bb0c2344fbbea5834fad66bf2554079834a41a
Sebastien-Raguideau <idaho90@live.fr>
2019-12-20 12:58:18 +0000

    fix issue with outputing selected bins

--- COG_pipe/scripts/this_should_not_be_a_script.py
+++ COG_pipe/scripts/this_should_not_be_a_script.py
@@ -14,9 +14,9 @@ args = parser.parse_args()
 
 bins = [basename(dirname(cogs_fn)) for cogs_fn in args.s if len(list(open(cogs_fn).readlines())) >= int(args.nb_orfs)]
 # we have been considering indistinctly bins and mags previously, we will now only retain mags
-mags = ["Bins_%s" % line.rstrip() for line in open(args.mags)]
+mags = ["Bin_%s" % line.rstrip() for line in open(args.mags)]
 merged_recipe = {line.rstrip().split('\t')[0]:line.rstrip().split('\t')[1:] for line in open(args.merge_recipe)}
 mags += [merged_bin for mergerd_bin,list_bins in merged_recipe.items() if (set(mags)&set(merged_recipe))!=set()]
 bins_selected=list(set(mags)&set(bins))
 with open(args.output, 'w') as out:
-  out.write("\n".join(bins))
+  out.write("\n".join(bins_selected))

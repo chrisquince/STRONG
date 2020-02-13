f4e7954aea9e8447df7e8083e646977ee3ca4fc1
chrisquince <cquince@warwick.ac.uk>
2020-02-13 16:34:08 +0000

    Changed bin inflation factor for CONCOCT to 2

--- COG_pipe/scripts/get_num_bin_ini.py
+++ COG_pipe/scripts/get_num_bin_ini.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("SCG_annotation",help="")
   args = parser.parse_args()
-  # we take a look at all the SCG, take the median over the 36 of them, and multiply that by 10
-  print(str(int(5*np.median(list(Counter([header.split(" ")[1] for header,seq in SimpleFastaParser(open(args.SCG_annotation))]).values())))))
+  # we take a look at all the SCG, take the median over the 36 of them, and multiply that by 2
+  print(str(int(2*np.median(list(Counter([header.split(" ")[1] for header,seq in SimpleFastaParser(open(args.SCG_annotation))]).values())))))

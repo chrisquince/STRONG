ed70361f5c0f3c92a7c7b7e29238db275043bc7f
chrisquince <cquince@warwick.ac.uk>
2020-03-15 14:13:43 +0000

    Fix some bugs

--- COG_pipe/scripts/ExtractSCGs.py
+++ COG_pipe/scripts/ExtractSCGs.py
@@ -13,18 +13,22 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("root_dir", help="directory with bin dirs in")
     parser.add_argument("cogs_list", help="list of COGs to collate from bins")
+    parser.add_argument("mags_list", help="list of good quality bins")
     parser.add_argument(
         "output_folder", help="output folder name, where do we want to put the sequences")
     args = parser.parse_args()
 #    import ipdb; ipdb.set_trace()
     with open(args.cogs_list, 'r') as f:
         core_cogs = set([x.rstrip() for x in f.readlines()])
+
+    with open(args.mags_list, 'r') as f:
+        mags = set([x.rstrip() for x in f.readlines()])
+
     corecog_bin = defaultdict(dict)
-    bins = set()
-    for filename in glob.iglob(args.root_dir + "/*/SCG.fna"):
-        bin_name = filename.split("/")[-2]
-        # print(bin_name)
-        bins.add(bin_name)
+    
+    for mag in mags:
+        filename = args.root_dir + "/Bin_" + mag + "/SCG.fna"
+        bin_name = "Bin_" + mag
         for header, seq in SPF(open(filename)):
             cogId = header.split(" ")[1]
             if cogId in core_cogs:
@@ -34,6 +38,8 @@ def main(argv):
                         corecog_bin[cogId][bin_name] = [header, seq]
                 else:
                     corecog_bin[cogId][bin_name] = [header, seq]
+
+
     path_output = args.output_folder+"/SCGs/"
     if not os.path.isdir(path_output):
         os.system("mkdir -p "+path_output)

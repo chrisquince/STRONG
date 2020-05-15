0b332ad3a9c834b135cb18da2f51076f55f0803c
Sebastien-Raguideau <idaho90@live.fr>
2020-05-15 20:13:30 +0100

    fix bug with SCG_in_Binse

--- COG_pipe/scripts/SCG_in_Bins.py
+++ COG_pipe/scripts/SCG_in_Bins.py
@@ -4,15 +4,57 @@ from Bio.SeqIO.FastaIO import SimpleFastaParser
 from collections import Counter, defaultdict
 import os
 
+def get_contig_split(contig_bed, contig, scg_bed, orf) : 
+    start,end = scg_bed[orf]
+    if end<start : 
+        (end,start) = (start,end)
+    best_overlap = []
+    for split_contig,start_split,end_split in contig_bed[contig] :
+        if end_split<start_split : 
+            (end_split,start_split) = (start_split,end_split)
+        # test if included in split 
+        is_start_in = (start_split<=start<=end_split)
+        is_end_in = (start_split<=end<=end_split)
+        # simplest case : 
+        if is_end_in&is_start_in :
+            return split_contig 
+        # shit overlap case
+        elif is_end_in|is_start_in :
+            best_overlap.append([split_contig,min(end_split,end)-max(start_split,start)])
+    return max(best_overlap,key=lambda x:x[1])[0]
 
-def main(Bin_file, Fasta_file, path, Table, path_list, flag_all_bins):
-    # which SCG on which contig with which sequence ?
+
+def main(Bin_file, Fasta_file, C10K_bed, orf_bed, path, Table, path_list, flag_all_bins):
+    # map contig + SCG to orf name and sequence
     Dico_contig_SCGSeq = defaultdict(lambda: defaultdict(list))
     for header, seq in SimpleFastaParser(open(Fasta_file)):
         contig = "_".join(header.split(" ")[0].split('_')[:-1])
         SCG = header.split(" ")[1]
         Dico_contig_SCGSeq[contig][SCG].append([header, seq])
-    # which SCG in which bins
+    # ---- deal with case where we have split contigs  ------
+    # get split contigs bed infos 
+    contig_bed = defaultdict(list)
+    for line in open(C10K_bed) :
+        contig,start,end,split_contig = line.rstrip().split("\t")
+        NB = len(contig.split("."))
+        if (len(split_contig.split("."))==NB)|(contig not in Dico_contig_SCGSeq) :
+            continue
+        contig_bed[contig].append([split_contig,int(start),int(end)])
+    # get SCG orf definitions 
+    scg_bed = {line.rstrip().split("\t")[3]:list(map(int,line.rstrip().split("\t")[1:3])) for line in open(orf_bed) if line.rstrip().split("\t")[0] in Dico_contig_SCGSeq}
+    # for each split contigs assign its SCG 
+    Dico_contig_SCGSeq_splits = defaultdict(lambda: defaultdict(list))
+    for contig, dict_scg in Dico_contig_SCGSeq.items() : 
+        if contig not in contig_bed :
+            continue
+        for scg, list_header in dict_scg.items() :
+            for header, seq in list_header :
+                orf = header.split(" ")[0]
+                split_contig = get_contig_split(contig_bed, contig, scg_bed, orf)
+                Dico_contig_SCGSeq_splits[split_contig][scg].append([header, seq])
+    Dico_contig_SCGSeq.update(Dico_contig_SCGSeq_splits)
+
+    # map bins to SCG
     Dico_bins_SCG = defaultdict(lambda: defaultdict(list))
     Dico_bins_nbcontigs = defaultdict(int)
     for index, line in enumerate(open(Bin_file)):
@@ -24,7 +66,7 @@ def main(Bin_file, Fasta_file, path, Table, path_list, flag_all_bins):
             for SCG, list_values in Dico_contig_SCGSeq[contig].items():
                 Dico_bins_SCG[Bin][SCG] += list_values
 
-# --------------- SCG output for concoct refine------------------------------------------------------------
+# --------------- SCG output for concoct refine-----------------------------------
     if Table:
         List_SCG = sorted({key for dict in Dico_bins_SCG.values()
                            for key in dict.keys()})
@@ -72,6 +114,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("Bin_file", help="Binning result file, csv, first column is the contig, second column is the bin")
     parser.add_argument("SCG_Fasta", help="fasta file of Orfs annotated as SCG")
+    parser.add_argument("orf_bed", help="bed file of orfs definition : needed to handle concoct cut contigs")
+    parser.add_argument("C10K_bed", help="bed file of cut contigs definition : needed to handle concoct cut contigs ")
     parser.add_argument("-f", help="path to where you want to store the bins folders")
     parser.add_argument("-all", help="same as -f, but output for all bins, not just mags")
     parser.add_argument("-l", help="just output the list of MAG in this file")
@@ -79,6 +123,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     Bin_file = args.Bin_file
     Fasta_file = args.SCG_Fasta
+    C10K_bed = args.C10K_bed
+    orf_bed = args.orf_bed
     path = ""
     Table = ""
     path_list = ""
@@ -92,4 +138,4 @@ if __name__ == "__main__":
         Table = args.t
     if args.l:
         path_list = args.l
-    main(Bin_file, Fasta_file, path, Table, path_list, flag_all_bins)
+    main(Bin_file, Fasta_file, C10K_bed, orf_bed, path, Table, path_list, flag_all_bins)

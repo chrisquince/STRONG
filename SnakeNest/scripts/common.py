1d884e8ea7ce13cc78b2fd6f09ae628b8b19c9f8
chrisquince <cquince@warwick.ac.uk>
2020-11-04 11:09:54 +0000

    Added optional BP params

--- SnakeNest/scripts/common.py
+++ SnakeNest/scripts/common.py
@@ -23,6 +23,7 @@ default_values = {
     "bayespaths": {"min_orf_number_to_merge_bins": 10,
                    "min_orf_number_to_run_a_bin": 10,
                    "percent_unitigs_shared": 0.1,
+                   "optional_args": "",
                    "nb_strains": 16,
                    "nmf_runs":10,
                    "max_giter":4},

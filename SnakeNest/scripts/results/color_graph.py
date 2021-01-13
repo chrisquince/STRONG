2eec8e78341a5e9b23ce3735823ea3fbf1099965
Sebastien-Raguideau <idaho90@live.fr>
2021-01-13 09:51:49 +0000

    refactoring color script

--- SnakeNest/scripts/results/color_graph.py
+++ SnakeNest/scripts/results/color_graph.py
@@ -6,25 +6,38 @@ from os.path import basename
 from collections import Counter, defaultdict
 from Bio.SeqIO.FastaIO import SimpleFastaParser as sfp
 
-# 20 "distinguishable" colours
-color_scheme = ["#0075DC","#FFCC99", "#2BCE48", "#F0A3FF", "#993F00", "#4C005C", "#808080", "#94FFB5", "#8F7C00", "#9DCC00","#C20088", "#003380", "#FFA405", "#FFA8BB", "#426600", "#FF0010", "#5EF1F2", "#00998F", "#740AFF", "#990000", "#FFFF00"]
 
+# black magic, look away (mostly padding issue though)
+def int_to_hex(x):
+    hexagram = hex(int(x))[2:].upper()
+    if hexagram == "0":
+        return "00"
+    else: 
+        if len(hexagram) == 1:
+            return "0"+hexagram
+        else:
+            return hexagram
 
 def merge_color(colors):
-    # black magic, look away (mostly padding issue though)
-    def int_to_hex(x):
-        hexagram = hex(int(x))[2:].upper()
-        if hexagram == "0":
-            return "00"
-        else: 
-            if len(hexagram) == 1:
-                return "0"+hexagram
-            else:
-                return hexagram
     # convert hexa to int, get mean 
     mean_color = np.array([[int(color[1:3], 16), int(color[3:5], 16), int(color[5:], 16)] for color in colors]).mean(0)
     merged_color = "#"+"".join([int_to_hex(value) for value in mean_color])
     return merged_color
+
+def generate_color_scheme(sorted_strains):
+    # 20 "distinguishable" colours
+    color_scheme = ["#0075DC","#FFCC99", "#2BCE48", "#F0A3FF", "#993F00", "#4C005C", "#808080", "#94FFB5", "#8F7C00", "#9DCC00","#C20088", "#003380", "#FFA405", "#FFA8BB", "#426600", "#FF0010", "#5EF1F2", "#00998F", "#740AFF", "#990000", "#FFFF00"]
+    Nb_color = len(sorted_strains)
+    if Nb_color>len(color_scheme):
+        nb_cycle = Nb_color%len(color_scheme)+1
+        increment = 255./nb_cycle
+        grey_grad = ["#"+"".join(3*[int_to_hex(nb*increment)]) for nb in range(nb_cycle)]
+        new_color_scheme = [merge_color([col,grey]) for grey in grey_grad for col in color_scheme]
+    else:
+        new_color_scheme = color_scheme
+    strain_to_color = {strain:new_color_scheme[index] for index,strain in enumerate(sorted_strains)}
+    return strain_to_color
+
 
 def color_gfa(gfa_file,contig_to_color):
     colored_gfa=""
@@ -45,7 +58,8 @@ def color_gfa(gfa_file,contig_to_color):
 
 def main(gfa_file, contig_to_strains, output, split):
     sorted_strains=sorted({strain for strains in contig_to_strains.values() for strain in strains if "NA" not in strain})
-    contig_to_color={contig:merge_color([color_scheme[sorted_strains.index(strain)] for strain in strains]) for contig,strains in contig_to_strains.items()}
+    strain_to_color = generate_color_scheme(sorted_strains)
+    contig_to_color={contig:merge_color([strain_to_color[strain] for strain in strains]) for contig,strains in contig_to_strains.items()}
 
     # next add this information to the gfa file 
     colored_gfa = color_gfa(gfa_file,contig_to_color)
@@ -84,7 +98,7 @@ if __name__ == "__main__":
             for contig in line[1:]:
                 contig_to_strains[contig].append(strain)
     else :
-        contig_to_strains={line.split()[0]:line.split()[1:] for line in open(args.a)}
+        contig_to_strains={line.split(",")[0]:line.rstrip().split(",")[1:] for line in open(args.a)}
 
     main(args.gfa_file,contig_to_strains,args.output,args.s)
 
